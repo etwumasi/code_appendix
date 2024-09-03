7cfc61a56fdf09836d100ca8880250e04d8b2456
etwumasi <etwumasi.coe@knust.edu.gh>
2024-09-03 12:45:28 +0000

    Add files via upload

---
+++ main.m
@@ -0,0 +1,49 @@
+% Developed in MATLAB R2017b
+% Source codes demo version 1.0
+% _____________________________________________________
+
+%  Author, inventor and programmer: Iraj Naruei and Farshid Keynia,
+%  e-Mail: irajnaruei@iauk.ac.ir , irajnaruei@yahoo.com
+% _____________________________________________________
+%  Co-author and Advisor: Farshid Keynia
+%
+%         e-Mail: fkeynia@gmail.com
+% _____________________________________________________
+%  Co-authors: Amir Sabbagh Molahoseyni
+%        
+%         e-Mail: sabbagh@iauk.ac.ir
+% _____________________________________________________
+
+% You can find the COOT code at 
+% _____________________________________________________
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all 
+close all
+clc
+
+N=30; % Number of search agents
+
+Function_name='F1'; % Name of the test function 
+
+Max_iter=1000; % Maximum number of iterations
+
+% Load details of the selected benchmark function
+[lb,ub,dim,fobj]=Get_Functions_details(Function_name);
+
+[Convergence_curve,gBest,gBestScore]=COOT(N,Max_iter,lb,ub,dim,fobj);
+
+disp([' '])
+display(['The best location of COOT is: ', num2str(gBest)]);
+display(['The best fitness of COOT is: ', num2str(gBestScore)]);
+
+
+semilogy(Convergence_curve)
+xlabel('Iteration')
+ylabel('Function Value')
+legend('mCOA')
+title('Convergence Curve - mCOA')
+grid on
+
+
+
